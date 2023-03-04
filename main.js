910e54dc984e6497129e4f45103f7ed4da89715f
SaroshBot <103352606+SaroshBot@users.noreply.github.com>
2023-03-04 21:25:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var Sr = window.webkitSpeechRecognition;
+var recognition = new Sr();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var content= event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML=content;
+
+} 
